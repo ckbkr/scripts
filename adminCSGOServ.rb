3ba6294a5106c65a80f8164d26ed03d0661b89ef
ckbkr <lukas11235@gmx.de>
2016-04-10 19:24:04 +0200

    Added some useability

--- adminCSGOServ.rb
+++ adminCSGOServ.rb
@@ -2,8 +2,9 @@
 require 'ripl'
 require 'sqlite3'
 require 'json'
-require 'pry'
 require 'mysql' 
+require 'optparse'
+require 'pry'
 
 class MapEntries
 
@@ -21,17 +22,14 @@ class MapEntries
     @storeDBUser = "root"
     @storeDBPass = "lukas"
     @storeDBName = "store"
-
-    @filePrefix = String.new
-
+    
     @hddArms = Hash.new
     @hddSkins = Hash.new 
     @databaseSkins = Hash.new
   end
 
   def unpack(onlyMissing=false)
-    @currDir = Dir.pwd
-    @mapDir = Dir.pwd+"/"+@mapPath
+    @mapDir = @serverPath+"/"+@mapPath
     files = Dir.new(@mapDir)
     entries = files.entries
     counter = 0
@@ -39,23 +37,22 @@ class MapEntries
       counter = counter +1 
       if entry.end_with? ".bz2"
         if onlyMissing == true
-	  if File.exist? (@mapDir+"/"+entry[0,(entry.rindex ".bz2")])
+          if File.exist? (@mapDir+"/"+entry[0,(entry.rindex ".bz2")])
             p "Exists: " + entry[0,(entry.rindex ".bz2")]
             next
           end
         end
-	p counter.to_s + "/" + entries.size.to_s
+        p "Progress: " + counter.to_s + "/" + entries.size.to_s
         join = "cd " + @mapDir + " && " + @unpackCommand + entry
         ret = `#{join}`
-	p join
+        p join
       end
     end  
     p "Done unpacking"
   end    
 
   def pack(onlyMissing=false)
-    @currDir = Dir.pwd
-    @mapDir = Dir.pwd+"/"+@mapPath
+    @mapDir = @serverPath+"/"+@mapPath
     files = Dir.new(@mapDir)
     entries = files.entries
     counter = 0
@@ -63,21 +60,21 @@ class MapEntries
      counter = counter +1 
      if entry.end_with? ".bsp"
         if onlyMissing == true
-	  if File.exist? (@mapDir+"/"+entry +".bz2")
+          if File.exist? (@mapDir+"/"+entry +".bz2")
             p "Exists: " + entry+".bz2"
             next
           end
         end
-        p counter.to_s + "/" + entries.size.to_s
+        p "Progress: " + counter.to_s + "/" + entries.size.to_s
         join = "cd " + @mapDir + " && " + @packCommand + entry
         ret = `#{join}`
-	p join
+        p join
       end
     end
     p "Done packing"
   end
 
-  def createMapList
+  def createMapList(writeToFile=true)
     @mapDir = @serverPath+"/"+@mapPath
     mapList = File.open("maplist.txt", 'w')
     arrMapList = Array.new
@@ -85,22 +82,23 @@ class MapEntries
     files.entries.each do |entry|
       if entry.end_with? ".bsp"
         map = entry[0,(entry.rindex ".bsp")]
-	#p map
-        mapList << map + "\n"
-	arrMapList.push map
+        #p map
+        if writeToFile == true
+          mapList << map + "\n"
+        end
+        arrMapList.push map
       end
     end
     arrMapList
   end
-
 
   def createUMCList(mapList)
     db = SQLite3::Database.open @serverPath + "/" + @ckSurfDB 
     db.results_as_hash = true
     stm = db.prepare "SELECT mapname, count(CASE WHEN zonetype = 3 THEN 1 ELSE NULL END)+1 AS stages, \
                       (SELECT tier FROM ck_maptier b WHERE b.mapname = a.mapname) AS tier, count(DISTINCT zonegroup)-1 AS bonuses \
-		      FROM ck_zones a \
-		      GROUP BY mapname \
+              FROM ck_zones a \
+              GROUP BY mapname \
                       ORDER BY tier ASC; "
 
     rs = stm.execute
@@ -110,7 +108,7 @@ class MapEntries
     while (row = rs.next) do
         #puts row.join "\s"
         count = count + 1
-	#p row
+    #p row
         mapInfo[row['mapname']] = [  row['stages'], row['tier'],  row['bonuses'] ]
     end
 
@@ -127,9 +125,9 @@ class MapEntries
       if mapped != nil
         count = count + 1
         #p "Found in db: " + map 
-	#p mapped
-	mapTier = 0
-	if mapped[1] == nil 
+    #p mapped
+    mapTier = 0
+    if mapped[1] == nil 
           mapTier = 0
         else
           mapTier = mapped[1]
@@ -144,16 +142,16 @@ class MapEntries
     umcFile = File.open("umc_mapcycle.txt","w")
     umcFile << "\"umc_mapcycle\"\n"
     umcFile << "{\n"
-	
+    
     for i in 0..6
       if !(mappedResult[i].empty?) 
-        umcFile << "	\"Tier "+i.to_s+"\"\n"
-	umcFile << "	{\n"
-        umcFile << "		\"maps_invote\" \"0\"\n"
-	mappedResult[i].each do |res|
-          umcFile << "		\""+res[0]+"\"		{ \"display\"		\""+res[0]+" (T" + i.to_s
+        umcFile << "    \"Tier "+i.to_s+"\"\n"
+    umcFile << "    {\n"
+        umcFile << "        \"maps_invote\" \"0\"\n"
+    mappedResult[i].each do |res|
+          umcFile << "        \""+res[0]+"\"        { \"display\"        \""+res[0]+" (T" + i.to_s
           if res[1] > 1 
-	    umcFile << " " + res[1].to_s + "S"
+        umcFile << " " + res[1].to_s + "S"
           else
             umcFile << " L"
           end
@@ -162,7 +160,7 @@ class MapEntries
           end 
             umcFile << ")\" }\n"
         end
-        umcFile << "	}\n"
+        umcFile << "    }\n"
       end
     end
     umcFile << "}\n"
@@ -189,7 +187,7 @@ class MapEntries
             name = name + "_"
           end
           path.slice! removePrefix
-	  #p path
+      #p path
           @hddArms[name] = { "path" => path, "file" => entry }
           next 
         end
@@ -220,7 +218,7 @@ class MapEntries
         #p "Found arms for this model: " + ret.to_s
         ret[1]["arms"] = values["file"]
         ret[1]["armspath"] = values["path"]
-	values["matched"] = true
+    values["matched"] = true
       end
     end
     @hddArms.delete_if { |k,v| v["matched"] != nil }
@@ -262,7 +260,7 @@ class MapEntries
         #p "Found HDD representation of database: " + file
         ret[1]["present"] = true
         values["present"] = true
-	#binding.pry
+    #binding.pry
         if ret[1]["arms"] != nil && ret[1]["armspath"] != nil 
           if values["arms"] != nil && values["arms"] != ret[1]["armspath"] +"/"+ ret[1]["arms"] 
             #binding.pry
@@ -296,12 +294,10 @@ class MapEntries
     updateDBSkins( db, @updateEntries, @hddSkins, @deleteSkins )
   end
 
-  def mergeModels
-    db = openDatabase
+  def mergeModels(db)
     loadModels(db)
     listModels
     mergeHDDDatabase(db)
-    #binding.pry
   end
   
   def getSkinsCategoryFromDB(db)
@@ -380,14 +376,91 @@ class MapEntries
   def loadTrails
 
   end
+  
+  def entry
+    options = {}
+    OptionParser.new do |opts|
+      opts.banner = "Usage: " + File.basename(__FILE__) +" [options]"
+    
+      opts.on("-u", "--unpack", "unpack map bz2 files") do |u|
+        options[:unpack] = u
+      end
+      
+      opts.on("-p", "--pack", "pack map bz2 files") do |pa|
+        options[:pack] = pa
+      end
+      
+      opts.on("-e", "--existing", "do not unpack/pack files of which the map/archive file already exists") do |e|
+        options[:existing] = e
+      end
+      
+      opts.on("-l", "--list", "print a maplist.txt file according to the current maps") do |ml|
+        options[:maplist] = ml
+      end
+      
+      opts.on("-c", "--cycle", "create a umc mapcycle and print the umc cycle as well as the map cycle, uses ckSurf db to resolve tiers and stages") do |mc|
+        options[:mapcycle] = mc
+      end
+      
+      opts.on("-d", "--drop_skins", "delete all skins from the database") do |ds|
+        options[:dropskins] = ds
+      end
+      
+      opts.on("-i", "--import_skins", "import skins to the database from the mods/materials folder") do |is|
+        options[:importskins] = is
+      end
+    end.parse!
+    
+    if options.empty?
+      binding.pry
+    end
+   
+    if options[:unpack] == true 
+      if options[:existing] == true
+        unpack(true)
+      else
+        unpack
+      end
+    end
+    
+    if options[:pack] == true 
+      if options[:existing] == true
+        pack(true)
+      else
+        pack
+      end
+    end
+    
+    maplist = nil
+    if options[:maplist] == true 
+      maplist = createMapList
+    end
+    
+    mapcycle = nil
+    if options[:mapcycle] == true 
+      if maplist == nil
+        maplist = createMapList(false)
+      end
+      createUMCList(maplist)
+    end
+    
+    database = nil
+    
+    if options[:dropskins] == true 
+      db = openDatabase
+      deleteSkinsFromDB(db)
+    end
+    
+    if options[:importskins] == true 
+      if db == nil
+        db = openDatabase
+      end
+      mergeModels(db)
+    end
+    
+    
+  end
 
 end
 
-map = MapEntries.new
-#map.unpack
-#map.createMapList
-#map.createUMCList(map.createMapList)
-#map.mergeModels
-
-#map.listModels
-Ripl.start :binding => binding
+MapEntries.new.entry
